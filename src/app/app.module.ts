08a8b9a8bdba09188fd7100981b8e204f6fb6b34
dramosv90 <dariel.ramos.dv@gmail.com>
2019-08-01 17:12:58 -0400

    installing font library and testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee, faHome } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,14 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    library.add(faCoffee, faHome);
+  }
+}
